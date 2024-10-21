60110afecf33d9dc342ca1f63f5a1d7be04bf43a
greensgit <rajgreensomr@gmail.com>
2024-10-21 10:28:24 +0530

    new cide added

---
+++ src/org/dem/tes/Dummy.java
@@ -0,0 +1,17 @@
+package org.dem.tes;
+
+public class Dummy {
+
+public static void main(String[] args) {
+	
+	System.out.println("This is demo");
+	System.out.println("This is yuvaraj branch");
+}
+
+private void addToKart() {
+	System.out.println("This is anbarasu working on add to kart");
+}
+private void payment() {
+	System.out.println("This is Anish - working on Payment module");
+}
+}
